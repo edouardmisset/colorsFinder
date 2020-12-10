f38048c5efe312cc290e7302573522cd4812b3f8
Edouard Misset <edouardmisset@gmail.com>
2020-12-10 12:00:49 +0100

    Added colors as object and array to color the divs

--- script.js
+++ script.js
@@ -2,20 +2,73 @@ const body = document.getElementsByTagName("body")[0];
 
 function createDivs() {
   for (let index = 0; index < 24; index++) {
-  // create the div & input
-  let div = document.createElement("div");
-  let inputField = document.createElement("input");
-  // add eventlisteners
-  inputField.addEventListener("input", setBackgroundColor);
-  // place the div & input in the body
-  body.appendChild(div).appendChild(inputField);
-}
+    // create the div & input
+    let div = document.createElement("div");
+    let inputField = document.createElement("input");
+    // add eventlisteners
+    inputField.addEventListener("input", setBackgroundColor);
+    // place the div & input in the body
+    body.appendChild(div).appendChild(inputField);
+  }
 }
 
 createDivs();
 
 function setBackgroundColor(event) {
-  
   var selectedItem = event.target;
   selectedItem.parentNode.style.backgroundColor = selectedItem.value;
 }
+
+const gradesColors = {
+  "6a": "#FFCE9D",
+  "6a+": "	#FFBF80",
+  "6b": "#FFAA57",
+  "6b+": "	#FF8C1A",
+  "6c": "#CE6600",
+  "6c+": "	#663300",
+  "7a": "#A0E68E",
+  "7a+": "	#83DF6D",
+  "7b": "#68D94A",
+  "7b+": "	#48BD28",
+  "7c": "#24AD00",
+  "7c+": "	#146600",
+  "8a": "#7BB5C3",
+  "8a+": "	#61A6B8",
+  "8b": "#3B90A4",
+  "8b+": "	#2F7483",
+  "8c": "#02677F",
+  "8c+": "	#013D4B",
+  "9a": "#F999A4",
+  "9a+": "	#F77887",
+  "9b": "#F55467",
+  "9b+": "	#F21831",
+  "9c": "#C50017",
+  "9c+": "#80000F",
+};
+
+const gradesColorsArray = [
+  "#FFCE9D",
+  "#FFBF80",
+  "#FFAA57",
+  "#FF8C1A",
+  "#CE6600",
+  "#663300",
+  "#A0E68E",
+  "#83DF6D",
+  "#68D94A",
+  "#48BD28",
+  "#24AD00",
+  "#146600",
+  "#7BB5C3",
+  "#61A6B8",
+  "#3B90A4",
+  "#2F7483",
+  "#02677F",
+  "#013D4B",
+  "#F999A4",
+  "#F77887",
+  "#F55467",
+  "#F21831",
+  "#C50017",
+  "#80000F",
+];
