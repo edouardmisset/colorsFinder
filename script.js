c63fce1600be285d9c384e756b0cc00748eafec6
Edouard Misset <edouardmisset@gmail.com>
2020-12-10 12:50:54 +0100

    changed span to head and styled them

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 const gradesColors = {
     "6a": "#FFCE9D",
-    "6a+": "	#FFBF80",
+    "6a+": "#FFBF80",
     "6b": "#FFAA57",
-    "6b+": "	#FF8C1A",
+    "6b+": "#FF8C1A",
     "6c": "#CE6600",
-    "6c+": "	#663300",
+    "6c+": "#663300",
     "7a": "#A0E68E",
-    "7a+": "	#83DF6D",
+    "7a+": "#83DF6D",
     "7b": "#68D94A",
-    "7b+": "	#48BD28",
+    "7b+": "#48BD28",
     "7c": "#24AD00",
-    "7c+": "	#146600",
+    "7c+": "#146600",
     "8a": "#7BB5C3",
-    "8a+": "	#61A6B8",
+    "8a+": "#61A6B8",
     "8b": "#3B90A4",
-    "8b+": "	#2F7483",
+    "8b+": "#2F7483",
     "8c": "#02677F",
-    "8c+": "	#013D4B",
+    "8c+": "#013D4B",
     "9a": "#F999A4",
-    "9a+": "	#F77887",
+    "9a+": "#F77887",
     "9b": "#F55467",
-    "9b+": "	#F21831",
+    "9b+": "#F21831",
     "9c": "#C50017",
     "9c+": "#80000F",
   },
   body = document.getElementsByTagName("body")[0];
 
-function createDivs() {
+function createContent() {
   for (let key in gradesColors) {
     // create the div & input
     let div = document.createElement("div");
     let inputField = document.createElement("input");
-    let gradeName = document.createElement("span");
+    let gradeName = document.createElement("h1");
     // Style the div
     div.style.backgroundColor = gradesColors[key];
     // add eventlisteners
@@ -42,7 +42,7 @@ function createDivs() {
   }
 }
 
-createDivs();
+createContent();
 
 function setBackgroundColor(event) {
   var selectedItem = event.target;
