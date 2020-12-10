5bea9ecdcb09e26c13f6353e0f42e7203a689ab9
Edouard Misset <edouardmisset@gmail.com>
2020-12-10 11:43:31 +0100

    Working prototype

--- script.js
+++ script.js
@@ -2,23 +2,20 @@ const body = document.getElementsByTagName("body")[0];
 
 function createDivs() {
   for (let index = 0; index < 24; index++) {
-    // create the div & input
-    let div = document.createElement("div");
-    let inputField = document.createElement("input");
-    // add eventlisteners
-    inputField.addEventListener("enter", setBackgroundColor);
-    // place the div & input in the body
-    body.appendChild(div).appendChild(inputField);
-  }
+  // create the div & input
+  let div = document.createElement("div");
+  let inputField = document.createElement("input");
+  // add eventlisteners
+  inputField.addEventListener("input", setBackgroundColor);
+  // place the div & input in the body
+  body.appendChild(div).appendChild(inputField);
+}
 }
 
 createDivs();
 
 function setBackgroundColor(event) {
-  console.log(
-    "🚀 ~ file: script.js ~ line 20 ~ setBackgroundColor ~ event",
-    event
-  );
-  let element = event.target;
-  element.style.backgroundColor = element.value;
+  
+  var selectedItem = event.target;
+  selectedItem.parentNode.style.backgroundColor = selectedItem.value;
 }
